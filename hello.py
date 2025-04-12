deae24977f1ace3ca8d6f3fe4170afcec411448b
jaguarkid817 <methebestestofall@gmail.com>
2025-04-12 08:46:07 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@ import hashlib
 name = "Muhammad"
 print ("Hello " + name)
 
-function getpass()
+def getpass()
   print("fiona%s"%hashlib.md5("XXXYOURSERIALXXX\n".encode('utf-8')).hexdigest()[7:11])
 
 getpass()
